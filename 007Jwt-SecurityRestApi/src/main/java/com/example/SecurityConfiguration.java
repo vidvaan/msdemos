c831718a5b54b1b5ad2f1eb149e47dcb106cd50e
vidvaan <balajigvidvaan@gmail.com>
2024-01-08 09:30:29 +0530

    Add files via upload

--- 007Jwt-SecurityRestApi/src/main/java/com/example/SecurityConfiguration.java
+++ 007Jwt-SecurityRestApi/src/main/java/com/example/SecurityConfiguration.java
@@ -13,7 +13,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.filter.JwtRequestFilter;
 import com.example.service.UserService;
 
 @Configuration
@@ -22,13 +24,16 @@ public class SecurityConfiguration {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
                         .permitAll().anyRequest().authenticated());
-                
+          http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);     
         return http.build();
     }
 
